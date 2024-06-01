6a523cb046bd013c6ae6f65d3dd95062a1114c28
ritika <121666896+ritika2908@users.noreply.github.com>
2024-06-01 19:09:30 +0530

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+void print7(int n) 
+{
+    for (int i =0; i<n; i++){
+        // space
+        for (int j=0; j<n-i-1; j++){
+            cout<<" ";
+         
+        }
+
+        // star
+        for ( int j=0; j<2*i+1; j++){
+            cout<<"*";
+           
+        }
+
+        // space
+        for (int j=0; j<n-i-1; j++){
+            cout<<" ";
+            
+        }
+        cout<<endl;
+            
+        
+        }
+    }
+ 
+
+
+
+int main()
+{
+    int t;
+    cin >>t;
+    for (int i=0; i<t; i++)
+    { int n;
+    cin>>n;
+    print7(n);
+    
+    
+
+    }
+   
+    
+    
+
+
+}
